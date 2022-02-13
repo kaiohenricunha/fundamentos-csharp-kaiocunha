63c509e3c618af9a5a03cfdd9a03e13f3da1578f
Kaio Henrique Cunha <kaio.cunha@al.infnet.edu.br>
2022-02-13 20:53:32 -0300

    Add projects

---
+++ TP1/adicao/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Digite o primeiro número:");
+int input_1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Digite o segundo número:");
+int input_2 = Convert.ToInt32(Console.ReadLine());
+
+int resultado = input_1 + input_2;
+Console.WriteLine(input_1 + " + " + input_2 + " = " + resultado);
---
+++ TP1/divisao/divisao.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Digite o primeiro número:");
+int input_1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Digite o segundo número:");
+int input_2 = Convert.ToInt32(Console.ReadLine());
+
+try
+{
+    int resultado = input_1 / input_2;
+    Console.WriteLine(input_1 + " / " + input_2 + " = " + resultado);
+} catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
